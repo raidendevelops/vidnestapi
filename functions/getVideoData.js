5797ea4aa96ec6ecbb88c105866c302e0401446a
raidendevelops <raidensch0@gmail.com>
2025-03-06 19:39:39 -0500

    Update getVideoData.js

--- functions/getVideoData.js
+++ functions/getVideoData.js
@@ -261,7 +261,7 @@ exports.handler = async function(event) {
                     headers: {
                         'Access-Control-Allow-Origin': '*',
                         'Access-Control-Allow-Headers': 'Content-Type',
-                        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE,
+                        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
                     },
                     body: JSON.stringify({ error: 'Failed to fetch video' })
                 };
